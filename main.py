901801bcd039f3b32b8258ec985d08daa4b8a413
luckweber <thiago.p1989@gmail.com>
2016-08-21 01:58:49 -0300

    21/08/2016 01:59

--- main.py
+++ main.py
@@ -3,18 +3,46 @@ from pygame.locals import *
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((800, 600))
+#-------------------------BACKGROUND MUSIC-------------------------
+pygame.mixer.music.load('sound/Kevin_Hartnell_-_11_-_Earth_and_Stone.mp3')
+pygame.mixer.music.play(-1, 0.0)
+
+FPS = 30
+fpsClock = pygame.time.Clock()
+fpsClock.tick(FPS)
+
+DISPLAYSURF = pygame.display.set_mode((800, 600), 0, 32)
+
+megaImg = pygame.image.load('img/mega.png')
+megaX = 10;
+megaY = 10;
+direction = 'right'
 
 WHITE = (255, 255, 255)
-DISPLAYSURF.fill(WHITE)
+
+#-----------------ENCHER O DISPLAY COM BRANCO-----------------
+#DISPLAYSURF.fill(WHITE)
+
 RED = (255, 0, 0)
 pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 40, 40))
 
 pygame.display.set_caption('Hello World!')
 
-verdade = 1
+while True:
+	DISPLAYSURF.fill(WHITE)
+	
+	
+	if direction == 'right':
+		megaX +=5
+	elif direction == 'down':
+		megaY += 5
+	elif direction == 'left':
+		megaX -= 5
+	elif direction == 'up':
+		megaY -=4
+	
+	DISPLAYSURF.blit(megaImg, (megaX, megaY))
 
-while verdade == 1:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
